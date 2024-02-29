dbfd22b6d878878aaf4aa27ba31e2b4fc3f0f368
Einenlum <contact@einenlum.com>
2024-02-29 01:30:30 +0100

    Generate code

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import './style.css'
 import '@fontsource/coming-soon';
+import hljs from 'highlight.js/lib/core';
+import 'highlight.js/styles/atom-one-dark.css';
+import javascript from 'highlight.js/lib/languages/javascript';
+hljs.registerLanguage('javascript', javascript);
 
 window.onload = function() {
     const text = document.getElementById('text');
@@ -39,5 +43,48 @@ window.onload = function() {
 
         const results = document.getElementById('results');
         reproduceText(results);
+    });
+
+    document.getElementById('reset').addEventListener('click', (event) => {
+        event.preventDefault();
+        document.getElementById('results').textContent = '';
+        values = [];
+        start = null;
+        text.value = '';
+    });
+
+    const generateCode = () => {
+        return `
+const reproduceText = (element) => {
+    const values = ${JSON.stringify(values)};
+    const printingStart = new Date();
+
+    setInterval(() => {
+        const timePassed = new Date() - printingStart;
+
+        const value = values.filter((value) => value.time < timePassed).pop();
+
+        if (value) {
+            element.textContent = value.value;
+        }
+    }, 50);
+};
+`
+    };
+
+    document.getElementById('generate').addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const copyText = document.getElementById('copy');
+        copyText.classList.remove('hidden');
+        const code = document.getElementById('code');
+        code.textContent = generateCode();
+        hljs.highlightAll();
+    });
+
+    document.getElementById('copy').addEventListener('click', (event) => {
+        event.preventDefault();
+        const code = document.getElementById('code');
+        navigator.clipboard.writeText(code.textContent);
     });
 };
