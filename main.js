51999818904da7c3ea7af5f0027e05a6c63d59af
Einenlum <contact@einenlum.com>
2024-03-01 19:08:39 +0100

    disable textarea for a clearer ux

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ window.onload = function() {
         ])
     });
 
-    const replayText = (element) => {
+    const replayText = (element, values) => {
         element.textContent = '';
         const printingStart = new Date();
 
@@ -70,12 +70,16 @@ window.onload = function() {
 
         previewButton.classList.add('hidden');
         preview.classList.remove('hidden');
-        interval = replayText(preview);
+        // which means it IS disabled (yes, lol)
+        text.setAttribute('disabled', 'disabled');
+        interval = replayText(preview, values);
     });
 
     const reset = () => {
+        previewButton.classList.add('hidden');
         preview.classList.add('hidden');
         preview.textContent = '';
+        text.removeAttribute('disabled');
         setValues([]);
         start = null;
         text.value = '';
@@ -134,21 +138,7 @@ replayText(myElement, values);
         navigator.clipboard.writeText(code.textContent);
     });
 
-    const setHumanIsTheNewBlack = (element, values) => {
-        const printingStart = new Date();
-
-        setInterval(() => {
-            const timePassed = new Date() - printingStart;
-
-            const value = values.filter((value) => value.time < timePassed).pop();
-
-            if (value) {
-                element.textContent = value.value;
-            }
-        }, 50);
-    };
-
     const humanIsTheNewBlackValues = [{ "value": "H", "time": 0 }, { "value": "Hu", "time": 183 }, { "value": "Hum", "time": 286 }, { "value": "Huma", "time": 376 }, { "value": "Human", "time": 516 }, { "value": "Human ", "time": 669 }, { "value": "Human i", "time": 1016 }, { "value": "Human is", "time": 1371 }, { "value": "Human i", "time": 1703 }, { "value": "Human is", "time": 2087 }, { "value": "Human is ", "time": 2262 }, { "value": "Human is t", "time": 2359 }, { "value": "Human is th", "time": 2422 }, { "value": "Human is the", "time": 2472 }, { "value": "Human is the ", "time": 2562 }, { "value": "Human is the n", "time": 3025 }, { "value": "Human is the ne", "time": 3107 }, { "value": "Human is the new", "time": 3203 }, { "value": "Human is the new ", "time": 3295 }, { "value": "Human is the new b", "time": 3652 }, { "value": "Human is the new bl", "time": 3886 }, { "value": "Human is the new bla", "time": 3961 }, { "value": "Human is the new blac", "time": 4059 }, { "value": "Human is the new black", "time": 4213 }, { "value": "Human is the new black.", "time": 4280 }];
 
-    setHumanIsTheNewBlack(humanIsTheNewBlack, humanIsTheNewBlackValues);
+    replayText(humanIsTheNewBlack, humanIsTheNewBlackValues);
 };
